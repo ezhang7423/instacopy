0a6955b36d3f2c47ac50970ecea3ccac37e87a8d
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-20 13:03:18 -0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <h1>instacopy</h1>
-    <home></home>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import home from "./components/home.vue";
-
-export default {
-  name: "app",
-  components: {
-    home
-  }
-};
-</script>
-
 <style>
-@font-face {
-  font-family: Cookie;
-  src: url(assets/Cookie-Regular.ttf);
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-* {
-  font-family: Cookie;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
