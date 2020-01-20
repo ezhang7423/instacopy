37ba9bbff2e9108adbf49209b7e429917fa516f1
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-20 14:00:30 -0800

    positioning

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container">
-    <img class="logo" src="@/assets/ico.svg" />
-    <h1>Instacopy</h1>
-    <b-form-input
-      class="searche"
-      v-model="text"
-      placeholder="Search"
-    ></b-form-input>
+    <div class="left">
+      <img class="logo" src="@/assets/ico.svg" />
+      <h1 class="name">Instacopy</h1>
+    </div>
+    <div class="middle">
+      <b-form-input
+        class="searche"
+        v-model="text"
+        placeholder="Search"
+      ></b-form-input>
+    </div>
+    <div class="right">
+      <b-button variant="primary" class="login">Log In</b-button>
+      <b-link style="margin: 1vh 0 0 1vw">Sign Up</b-link>
+    </div>
   </div>
 </template>
 
@@ -23,6 +31,36 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  border-bottom: 1px solid gray;
+  max-width: 100vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2vh 0;
+}
+
+.right {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 20vw;
+}
+.left {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 20vw;
+}
+.login {
+  background-color: #3897f0;
+  line-height: 0.7;
+  margin-top: 1vh;
+  padding-bottom: 10px;
+  border: 1px solid transparent;
+  font-weight: 500;
+}
 .logo {
   height: 5vh;
 }
@@ -31,24 +69,22 @@ export default {
   src: url("../assets/Cookie-Regular.ttf");
 }
 .searche {
-  width: 5vw;
+  width: 15vw;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
   height: 4vh;
   margin-top: 1vh;
-}
-.container {
-  border-bottom: 1px solid gray;
-  max-width: 100vw;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-h1 {
-  font-family: Cookie;
-  margin: 1vh 0 0 0;
 }
 
 * {
   font-family: Segoe UI;
+  font-size: 14px;
+  font-weight: 500;
+}
+h1 {
+  font-size: 2.5rem;
+  font-family: Cookie;
+  margin: 1vh 0 0 0;
 }
 </style>
