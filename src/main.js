26eeaaa5229100bf4ba4b21e997610ec3937bf1c
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-20 13:31:44 -0800

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import store from "./store";
 import router from "./router";
 import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
