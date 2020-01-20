ccd8952e92bed1f1513b3fc664d3274001944629
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-20 13:13:43 -0800

    hmm

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import store from "./store";
+import router from "./router";
+import App from "./App.vue";
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
