e958d135cb9a18b8a55b612f284b57d17b9c0571
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-20 13:36:01 -0800

    add search

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <h1>Instacopy</h1>
+    <b-form-input
+      class="searche"
+      v-model="text"
+      placeholder="Search"
+    ></b-form-input>
   </div>
 </template>
 
@@ -16,11 +21,23 @@ export default {
   font-family: Cookie;
   src: url("../assets/Cookie-Regular.ttf");
 }
-
+.searche {
+  width: 5vw;
+  height: 4vh;
+}
 .container {
   border-bottom: 1px solid gray;
+  max-width: 100vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
-* {
+
+h1 {
   font-family: Cookie;
+}
+
+* {
+  font-family: Segoe UI;
 }
 </style>
